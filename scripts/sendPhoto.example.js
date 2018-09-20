9cfa5a09ec4833e4c8c46178164148e3655fc1bf
onelong <love@onelong.org>
2018-09-20 12:18:26 +0800

    chore: modify example

--- scripts/sendPhoto.example.js
+++ scripts/sendPhoto.example.js
@@ -5,12 +5,12 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function (robot) {
-  robot.hear(/send photo/i, function (res) {
+module.exports = robot => {
+  robot.hear(/send photo/i, res => {
     robot.emit('telegram:invoke', 'sendPhoto', {
       chat_id: res.message.room,
-      photo: fs.createReadStream(path.resolve(__dirname, '/image.png'))
-    }, function (error, response) {
+      photo: fs.createReadStream(path.resolve(__dirname, './image.png'))
+    }, (error, response) => {
       console.log(error)
       console.log(response)
     })
